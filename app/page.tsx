55293ac0e5794643eca518b6341e05e0d003b24e
Julia Nguyen <juliatnguyen05@gmail.com>
2026-02-28 13:26:50 -0800

    feat: add nav

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <h1>*cafe</h1>
-        <h1 className="font-[malevice]">hello</h1>
-      </main>
+    <div>
+      <nav className="flex justify-center sticky underline underline-offset-1">
+        *café
+      </nav>
+
     </div>
+    // <div className="flex min-h-screen items-center justify-center">
+    //   <nav className="flex justify-center">
+    //     <h1>*cafe</h1>
+    //   </nav>
+    //   <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+    //     {/* <h1 className="font-[malevice]">hello</h1> */}
+    //   </main>
+    // </div>
   );
 }
