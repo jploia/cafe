dd47e055d66c50f1c15a8249b19375961b1bff3d
Julia Nguyen <juliatnguyen05@gmail.com>
2026-03-01 06:37:55 -0800

    fix: increase delay on Stats

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -9,7 +9,7 @@ export default function Stats(){
         <motion.h2
             initial={{opacity: 0, x:1500, y: 20, rotate:15}}
             whileInView={{opacity: 1, x:0, y:0, rotate:0}}
-            transition={{duration:1.2, delay:.1}}
+            transition={{duration:1.2, delay:.4}}
             viewport={{once:true}}>
                 <div id="stats" className="flex mt-[20rem] w-[50%] ml-[45%]">
                 <h1 className="text-right text-[64px] leading-none"><span className="text-(--pink)">Strokes are the</span> LEADING <span className="text-(--pink)">cause of death for Americans.</span></h1>
